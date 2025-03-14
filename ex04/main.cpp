fbc28b0a8e2bf5ca489ff5b2940705f23a9d8b07
Tiago <tfa.antunes@gmail.com>
2025-03-14 23:32:45 +0000

    fix: bugs in ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 12:55:28 by tialbert          #+#    #+#             */
-/*   Updated: 2025/03/09 12:21:05 by tialbert         ###   ########.fr       */
+/*   Updated: 2025/03/14 23:30:37 by tialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,28 @@ static bool	make_file(std::string file, char **argv) {
 	std::string		new_file_name;
 	const char		*new_name;
 
+	if (**argv == 0)
+		return (false);
 	new_file_name = *argv;
 	new_file_name.append(".replace");
 	new_name = new_file_name.c_str();
 	file_cpy.open(new_name, std::ios::trunc);
 	if (!file_cpy.is_open())
 		return (false);
+
+	if (file.empty() == true) {
+		file_cpy.close();
+		return (true);
+	}
+
 	argv++;
 	i = 0;
 	while (i < file.length()) {
 		word_pos = file.find(argv[0], i);
 		if (word_pos != std::string::npos) {
 			file_cpy << file.substr(i, word_pos - i);
-			file_cpy << argv[1];
+			if (*argv[1] != 0)
+				file_cpy << argv[1];
 			i += (word_pos - i) + strlen(argv[0]);
 		}
 		else {
@@ -46,10 +55,14 @@ static bool	make_file(std::string file, char **argv) {
 int	main(int argc, char **argv) {
 	std::ifstream	file;
 	char			*memblock;
-	std::streampos	size;
+	unsigned int	size;
 
-	if (argc != 4) {
+	if (argc < 4) {
 		std::cout << "Not enough arguments!" << std::endl;
+		return (1);
+	}
+	else if (argc > 4) {
+		std::cout << "Too many arguments!" << std::endl;
 		return (1);
 	}
 
@@ -59,20 +72,36 @@ int	main(int argc, char **argv) {
 		std::cout << "Error opening file " << *argv << std::endl;
 		return (1);
 	}
-
-	size = file.tellg();	
-	memblock = new (std::nothrow) char[size];
-	if (memblock == NULL) {
-		file.close();
+	if (*argv[1] == 0) {
+		std::cout << "Argument cannot be an empty string" << std::endl;
 		return (1);
 	}
-	file.seekg(0, std::ios::beg);
-	file.read(memblock, size);
-	file.close();
 
-	if (!make_file(memblock, argv)) {
-		std::cout << "Error creating file." << std::endl;
-		return (1);
+	file.seekg(0, std::ios::end);
+	size = (unsigned int) file.tellg();	
+	if ((unsigned int) size > 0) {
+		memblock = new (std::nothrow) char[(unsigned int) size + 1];
+		if (memblock == NULL) {
+			file.close();
+			return (1);
+		}
+		file.seekg(0, std::ios::beg);
+		file.getline(memblock, (unsigned int) size + 1, 0);
+		file.close();
+
+		if (!make_file(memblock, argv)) {
+			std::cout << "Error creating file." << std::endl;
+			delete[] memblock;
+			return (1);
+		}
+		delete[] memblock;
+	}
+	else {
+		file.close();
+		if (!make_file("", argv)) {
+			std::cout << "Error creating file." << std::endl;
+			return (1);
+		}
 	}
 	
 	return (0);
